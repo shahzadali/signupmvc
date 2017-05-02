43d6839dfe48a25a5c81abb63b7a476fc4c926ae
Shahzad Ali <shahzad.ali@interpayafrica.com>
2017-05-02 12:58:18 +0500

    initial setup

---
+++ MVCSignupApp/MVCSignupApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCSignupApp.MvcApplication" Language="C#" %>
---
+++ MVCSignupApp/MVCSignupApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCSignupApp.Startup))]
+namespace MVCSignupApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
